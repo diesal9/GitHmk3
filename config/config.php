175b4a3199d8859ec993a4f49387d19a4c569d7e
diesal9 <charles.bocage@sjsu.edu>
2013-10-26 17:40:47 -0700

    added config files

---
+++ config/config.php
@@ -0,0 +1,41 @@
+<?php
+
+//the name of the db user
+global $username;
+$username = "xxxx";
+
+//the password of the db user
+global $password;
+$password = "xxxx";
+
+//the name of the db server
+global $server;
+$server = "localhost";
+
+//the name of the database on the server
+global $databaseName;
+$databaseName = "cbocage_hmk3";
+
+//the name of the login session variable
+global $userId;
+$userId = "userId";
+
+//an array of supported controllers
+global $controllersSupported;
+$controllersSupported = array("main");
+
+//an array of supported views
+global $viewsSupported;
+$viewsSupported = array("nonloggedin", "limerick");
+
+//the title of the blog site
+global $siteTitle;
+$siteTitle = "Looney Limericks";
+
+//an array of files to include
+global $filesToInclude;
+$filesToInclude = array("classes/C_Hw3.inc", "classes/C_Database.inc", 
+    "models/authenticate.php", "models/limerick_model.php", 
+   "classes/C_Limerick.inc");
+
+?>
---
+++ config/readme.txt
@@ -0,0 +1,4 @@
+1.  Set the config option short_open_tag in php.ini to 0 or Off and restart 
+    Apache
+2.  make sure you have the ability to create/delete files and directories in 
+    the entries folder
